952d21ed2a1058874a656842bf2411a61765af64
Hardik_Nimbarte009 <110125134+Hardik-Nimbarte@users.noreply.github.com>
2024-02-11 08:27:18 +0000

    making substring and slice of the string

--- 01_lecture/string.js
+++ 01_lecture/string.js
@@ -8,6 +8,13 @@ const repoCount=50
 console.log('my name is ${name} and repocount is ${repoCount}');
 
 const gameName= new String('hitesh')
-console.log(gameName[0])
-console.log(gameName.length)
-console.log(gameName.toUpperCase())
+// console.log(gameName[0])
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.char )
+
+const newName=gameName.substring(1,4);//here we print from 
+console.log(newName)
+
+const anotherString=gameName.slice(4,5)//it takes the slice of that string
+console.log(anotherString)
