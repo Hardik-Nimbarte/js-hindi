819d4c86c95578c7135ab9ac485d8a14229b484c
Hardik_Nimbarte009 <110125134+Hardik-Nimbarte@users.noreply.github.com>
2024-02-11 04:56:28 +0000

    new function

--- 01_lecture/string.js
+++ 01_lecture/string.js
@@ -2,4 +2,12 @@ const jyesh ="hello"+ "world"
 //console.log(jyesh)
 const name= "hitesh"
 const repoCount=50
-console.log(name + repoCount  +"value")
+//console.log(name + repoCount  +"value")
+
+//using backtick
+console.log('my name is ${name} and repocount is ${repoCount}');
+
+const gameName= new String('hitesh')
+console.log(gameName[0])
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
