e01ac57b2a62eb4870c341abfcd07a4fe131307c
Hardik_Nimbarte009 <110125134+Hardik-Nimbarte@users.noreply.github.com>
2024-02-19 19:06:03 +0000

    freeze

--- basics/object.js
+++ basics/object.js
@@ -6,7 +6,15 @@ const JsUser ={
     name:"Hardik",
     age:"18",
     lacation:"jaipur",
-    isLoggedIn:false
+    isLoggedIn:false,
+    email:"hardik@google.com"
 }
 
-console.log(JsUser.name)
+//console.log(JsUser.name)
+//JsUser.email="saurabh@google.com"
+console.log(JsUser.email)
+//to freeze aything that must not get changed in the future 
+//the changes will not get propogted
+Object.freeze(JsUser.email)
+JsUser.email="saurabh@google.com"
+console.log(JsUser)
