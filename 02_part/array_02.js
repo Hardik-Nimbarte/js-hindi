36c24de1ca8978a891c5ef6348395ff6e14dccb2
Hardik_Nimbarte009 <110125134+Hardik-Nimbarte@users.noreply.github.com>
2024-02-19 15:31:43 +0000

    checking the  element  is array or not

--- 02_part/array_02.js
+++ 02_part/array_02.js
@@ -11,3 +11,13 @@ console.log(all_new_hero)
 const another_array=[1,2,3,[4,5],7,[8,5,4,[4,5]]]
 const real_another_array=another_array.flat(Infinity)
 console.log(real_another_array)
+
+//checking wether the given element is array or not 
+console.log(Array.isArray("Hardik"))
+console.log(Array.from("Aman"))
+console.log(Array.from({name:"hitesh"}))//intersting
+
+let score1=100
+let score2=200
+let score3=300
+console.log(Array.of(score1,score2,score3))
