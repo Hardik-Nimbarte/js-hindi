d8d9f00668539aeec84de1287ab04070eef26c83
Hardik_Nimbarte009 <110125134+Hardik-Nimbarte@users.noreply.github.com>
2024-02-19 15:13:00 +0000

    array in js

---
+++ 02_part/array_01.js
@@ -0,0 +1,31 @@
+//array
+const myArray=[0,1,2,3,4]
+const myHeros=["shaktiman","naagraaj"]
+const myArray2=new Array(1,2,34,5,6)
+
+// console.log(myArray[2])
+
+// //array methods
+
+// myArray.push(234)//adding at the last 
+// myArray.pop()//delete the last array
+// myArray.unshift(9)//adds at the starting 
+// myArray.shift()//dele the element at the starting
+
+// console.log(myArray.includes(9))//check whether the 
+//                                 //element present in the array or not
+
+// console.log(myArray.indexOf(9))//gives the index of the element
+
+// console.log(myArray)
+
+//slice, splice
+console.log("A ",myArray)
+
+const myn1 = myArray.slice(1,3)
+console.log(myn1)
+console.log("B",myArray)
+
+const myn2= myArr.splice(1,3)
+console.log("C",myArray)
+console.log(myn2)
---
+++ 02_part/array_02.js
@@ -0,0 +1,13 @@
+const marvel_heros=["thor", "iron man", "spider man"]
+const dc_heros=["batman","superman","flash"]
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros)
+const allHero=marvel_heros.concat(dc_heros)//instesd of concate we can use spread(...)
+console.log(allHero)
+
+const all_new_hero=[...marvel_heros,...dc_heros]
+console.log(all_new_hero)
+
+const another_array=[1,2,3,[4,5],7,[8,5,4,[4,5]]]
+const real_another_array=another_array.flat(Infinity)
+console.log(real_another_array)
