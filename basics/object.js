8cb99330036d5489cc87d3fe2ea720d3e176c491
Hardik_Nimbarte009 <110125134+Hardik-Nimbarte@users.noreply.github.com>
2024-02-20 10:56:06 +0000

    objects and important note

--- basics/object.js
+++ basics/object.js
@@ -9,12 +9,20 @@ const JsUser ={
     isLoggedIn:false,
     email:"hardik@google.com"
 }
-
 //console.log(JsUser.name)
 //JsUser.email="saurabh@google.com"
 console.log(JsUser.email)
 //to freeze aything that must not get changed in the future 
 //the changes will not get propogted
-Object.freeze(JsUser.email)
+//Object.freeze(JsUser.email)
 JsUser.email="saurabh@google.com"
-console.log(JsUser)
+//console.log(JsUser)
+
+JsUser.greeting = function(){
+    console.log("Hello users")
+}
+JsUser.greetingTwo = function(){
+    console.log('Hello users ${this.name}')
+}
+
+console.log(JsUser.greeting)
