d965e6113deaf1c48bb26f8cea78925483c164e3
Hardik_Nimbarte009 <110125134+Hardik-Nimbarte@users.noreply.github.com>
2024-02-20 14:45:20 +0000

    object nesting

---
+++ basics/02_onject.js
@@ -0,0 +1,11 @@
+//declare the object 
+
+//const tinderUser= new Object()
+const tinderUser={}
+tinderUser.id="hardboy"
+tinderUser.name="ram"
+tinderUser.isLoggedIn=false
+//console.log(tinderUser)
+
+//nesting of the object
+const regula
